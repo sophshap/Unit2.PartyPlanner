86d17cad554c23d75db9ac773709ff6138345c22
sophshap <sophieashapiro@gmail.com>
2023-12-01 15:19:23 -0500

    working code, added delete and update

--- script.js
+++ script.js
@@ -1,84 +1,154 @@
-const API_URL = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-GHP-ET-WEB-FT-SF/events"
+const COHORT = "2310-GHP-ET-WEB-FT-SF";
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
 
 const state = {
     events: [],
-}
+};
 
+const eventList = document.querySelector("#events");
 
+const addEventForm = document.querySelector("#addEvent");
+addEventForm.addEventListener("submit", addEvent);
 
-const addEventForm = document.querySelector("addEvent")
-
-// need to get the events
-// then u can render them
-// but call for render up front
-
+/**
+ * Sync state with the API and rerender
+ */
 async function render() {
-    await getEvents()
-    renderEvents()
+    await getEvents();
+    renderEvents();
 }
+render();
 
-render()
-
+/**
+* Update state with events from API
+*/
 async function getEvents() {
     try {
-        const response = await fetch(API_URL)
-        const json = await response.json()
-        console.log(json)
-        state.events = json.data
+        const response = await fetch(API_URL);
+        const json = await response.json();
+        console.log(response)
+        state.events = json.data;
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
 }
 
+/**
+ * Render events from state
+ */
 function renderEvents() {
-
+    console.log(state.events)
     if (!state.events.length) {
-        const eventList = document.createElement("td")
-        eventList.textContent = "No events!"
-        return
+        eventList.innerHTML = "<li>No events.</li>";
+        return;
     }
 
-    const eventContainer = document.querySelector("#events")
-    const eventElement = state.events.map((event) => {
-        const element = document.createElement("tr")
-        const name = document.createElement("td")
-        const nameText = document.createTextNode(event.name)
-        name.appendChild(nameText)
+    const eventCards = state.events.map((event) => {
+        const eventElement = document.createElement("div");
+        eventElement.classList.add('event-card');
 
-        const description = document.createElement("td")
-        const descriptionText = document.createTextNode(event.description)
-        description.appendChild(descriptionText)
+        const isoStr = event.date;
+        const date = new Date(isoStr);
+        const time = date.toTimeString();
 
+        eventElement.innerHTML = `
+        <div>${event.name}</div>
+        <div>${event.description}</div>
+        <div>${event.date}</div>
+        <div>${time}</div>
+        <div>${event.location}</div>
+        
+      `;
 
-        const date = document.createElement("td")
-        const dateText = event.date.split("T")
-        const dateString = dateText[0] // index 1 is the time
-        const newDate = new Date(dateString).toLocaleDateString()
-        const dateOnly = document.createTextNode(newDate)
-        date.appendChild(dateOnly)
+        //delete button
+        const deleteButton = document.createElement('button')
+        deleteButton.textContent = "Delete Event"
+        eventElement.append(deleteButton)
 
+        deleteButton.addEventListener("click", () => deleteEvent(event.id))
 
-        const time = document.createElement("td")
-        const timeString = dateText[1]
-        const timeOnly = document.createTextNode(timeString)
-        time.appendChild(timeOnly)
+        //edit button
+        const editButton = document.createElement('button')
+        editButton.textContent = "Edit Event"
+        eventElement.append(editButton)
 
+        editButton.addEventListener("click", () => updateEvent(event.id))
 
-        const location = document.createElement("td")
-        const locationText = document.createTextNode(event.location)
-        location.appendChild(locationText)
+        return eventElement;
+    });
 
-
-
-        element.appendChild(name)
-        element.appendChild(description)
-        element.appendChild(date)
-        element.appendChild(time)
-        element.appendChild(location)
-
-        eventContainer.appendChild(element)
-
-    })
-
+    eventList.replaceChildren(...eventCards);
 }
 
+/**
+ * Ask the API to create a new event based on form data
+ * @param {Event} event
+ */
+async function addEvent(event) {
+    event.preventDefault();
+    const time = addEventForm.time.value;
+    const date = addEventForm.date.value;
+    const dateCombine = `${time} ${date}`;
+    const newDate = new Date(dateCombine);
+    const fDate = newDate.toISOString();
+
+    try {
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                name: addEventForm.name.value,
+                description: addEventForm.description.value,
+                date: fDate,
+                location: addEventForm.location.value,
+            }),
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to create event");
+        }
+
+        render();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// DELETE
+async function deleteEvent(id) {
+    try {
+        const response = await fetch(`${API_URL}/${id}`, {
+            method: "DELETE",
+        })
+        render()
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// UPDATE 
+async function updateEvent(id) {
+    const time = addEventForm.time.value;
+    const date = addEventForm.date.value;
+    const dateCombine = `${time} ${date}`;
+    const newDate = new Date(dateCombine);
+    const fDate = newDate.toISOString();
+    try {
+        const response = await fetch(`${API_URL}/${id}`, {
+            method: 'PUT',
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify({
+                name: addEventForm.name.value,
+                description: addEventForm.description.value,
+                date: fDate,
+                location: addEventForm.location.value,
+            }),
+        });
+        if (!response.ok) {
+            throw new Error("Failed to create event");
+        }
+        render();
+    } catch (error) {
+        console.error(error);
+    }
+}
